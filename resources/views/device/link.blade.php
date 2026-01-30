cae78c505250ee93ddff1a48d3b745797b718def
Mathias Placho <mathias@simplease.at>
2026-01-30 22:48:02 +0100

    Updated device linking form

--- resources/views/device/link.blade.php
+++ resources/views/device/link.blade.php
@@ -43,7 +43,7 @@
         <script>
             document.getElementById('user_code').addEventListener('input', function(e) {
                 let value = e.target.value.toUpperCase().replace(/[^A-Z0-9]/g, '');
-                if (value.length > 4) {
+                if (value.length >= 4) {
                     value = value.slice(0, 4) + '-' + value.slice(4, 8);
                 }
                 e.target.value = value;
