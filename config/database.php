4a187ab3f01b0beb70330c3ca4f14b9d401e355e
Mathias Placho <mathias@simplease.at>
2026-02-03 07:57:56 +0100

    Fixed redis prefix

--- config/database.php
+++ config/database.php
@@ -177,7 +177,7 @@ return [
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
             'persistent' => true,
-            'prefix' => null,
+            'prefix' => '',  // Empty to avoid double-prefix with cache.php
             'serializer' => extension_loaded('igbinary') ? Redis::SERIALIZER_IGBINARY : Redis::SERIALIZER_PHP,
             'compression' => Redis::COMPRESSION_LZ4,
             'compression_level' => 3,
